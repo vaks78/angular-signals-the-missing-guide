71333e0c8d47d6597d05a35a99bb75b12365ea05
Seryozha Vaks <vaks78@gmail.com>
2026-01-03 19:16:07 +0200

    04 injection context finished

--- practice/04_injection-context/src/app/components/counter/counter.component.ts
+++ practice/04_injection-context/src/app/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, effect, EffectRef, inject, Injector, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,25 +8,40 @@ import { Component, signal } from '@angular/core';
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
-  readonly counter = signal(0);
+  readonly #counter = signal(0);
 
   // 1. Inject the destroyRef here
+  readonly #dr = inject(DestroyRef);
   // 2. Inject the injector here
+  readonly #injector = inject(Injector);
+  #loggingEff = signal<EffectRef | null>(null);
 
   constructor() {
     const int = setInterval(() => {
-      this.counter.update(v => v + 1);
+      this.#counter.update(v => v + 1);
     }, 1000);
 
     // 3. Use the destroyRef to clear the interval (use `clearInterval(int)`)
+    this.#dr.onDestroy(() => {
+      clearInterval(int);
+    });
   }
 
   // 4. Create an effect when clicking a button
   startEffect() {
+    if (this.#loggingEff()) {
+      return;
+    }
+    this.#loggingEff.set(effect(() => {
+      console.log(`Counter value: ${this.#counter()}`);
+    }, 
+    { injector: this.#injector }));
   }
 
   // 5. Stop the effect when clicking another button
   stopEffect() {
+    this.#loggingEff()?.destroy();
+    this.#loggingEff.set(null);
   }
 
 }
